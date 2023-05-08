3844d697ae776b3f0bd024ee30d7696495fce9f9
WisPc <Semih Karduz@wis.pc>
2023-05-08 11:12:17 +0300

    Proje dosyası ekle.

---
+++ DentistCalendar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DentistCalendar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
